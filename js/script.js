594f8f2a7e237e7b606797fb371b10d408f142b1
Ryan Lertola <rlertola@gmail.com>
2018-12-01 19:03:53 -0600

    Finished all the requirements.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
 // Put ul items in array
 const liItems = document.querySelectorAll('.student-item');
 
@@ -27,14 +24,63 @@ searchDiv.appendChild(searchInput);
 searchDiv.appendChild(searchButton);
 header.appendChild(searchDiv);
 
-
-
-const search = (e) => {
+const searchFromInput = (e) => {
   const searchValue = e.target.value;
-  console.log(searchValue);
-
+  filterSearch(searchValue);
 }
-searchInput.addEventListener('keyup', search);
+
+const searchFromButton = (e) => {
+  const searchValue = searchInput.value;
+  filterSearch(searchValue);
+}
+
+const filterSearch = (inputValue) => {
+  if (inputValue === '') {
+    removeError();
+    showPage(1);
+    appendPageLinks(liItems);
+  } else {
+    let results = [];
+    [...liItems].forEach((item) => {
+      if (item.textContent.includes(inputValue)) {
+        item.style.display = '';
+        results.push(item);
+      } else {
+        item.style.display = 'none';
+      }
+    })
+
+    // If the search returns no results.
+    if (results.length === 0) {
+      showError();
+      appendPageLinks([]);
+    } else {
+      removeError();
+      appendPageLinks(results);
+    }
+  }
+}
+
+// Error message for no search results.
+const showError = () => {
+  // So the error doesn't keep repeating.
+  if (document.querySelector('.error')) {
+    removeError();
+  }
+  const p = document.createElement('p');
+  p.className = 'error';
+  p.textContent = `Oops, we couldn't find what you're looking for`
+  header.appendChild(p);
+}
+
+const removeError = () => {
+  if (document.querySelector('.error')) {
+    header.removeChild(document.querySelector('.error'));
+  }
+}
+
+searchInput.addEventListener('keyup', searchFromInput);
+searchButton.addEventListener('click', searchFromButton);
 
 
 // Hides all items except 10 depending on page number.
@@ -55,21 +101,26 @@ const showPage = (pageNumber) => {
 }
 
 
-// generate, append, and add functionality to the pagination buttons. Runs only once.
-const appendPageLinks = () => {
+// Generate, append, and add functionality to the pagination buttons. Runs only once.
+const appendPageLinks = (itemsArr) => {
+  // Checks if page buttons are there and removes them if they are.
+  if (div.children) {
+    [...div.children].forEach((child) => {
+      div.removeChild(child);
+    })
+  }
+
   // How many pages there should be.
-  const numberOfPages = Math.ceil(liItems.length / 10);
+  const numberOfPages = Math.ceil(itemsArr.length / 10);
   for (let pageNumber = 1; pageNumber <= numberOfPages; pageNumber++) {
     const li = document.createElement('li');
-
-    // First page is highlighted when page loads.
+    // First page is highlighted when page loads or when search is blank.
     const a = document.createElement('a');
     if (pageNumber === 1) {
       a.className = 'active';
     } else {
       a.className = 'inactive';
     }
-
     // Page loads to the top when a new page is selected.
     a.href = '#';
     a.textContent = pageNumber;
@@ -96,4 +147,4 @@ const changePage = (e) => {
 
 // Shows page 1 and buttons when page is first loaded.
 showPage(1);
-appendPageLinks();
+appendPageLinks(liItems);
