7ccfcec57173e938c428929ba19120fea0a03e83
Ryan Lertola <rlertola@gmail.com>
2018-12-01 21:51:16 -0600

    Finished all for extra credit.

---
+++ README.md
@@ -0,0 +1,5 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+
--- js/script.js
+++ js/script.js
@@ -3,148 +3,160 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
+// Helper function to create elements.
+const createElement = (name, prop, val) => {
+  const element = document.createElement(name);
+  element[prop] = val;
+  return element;
+}
+
 // Put ul items in array
 const liItems = document.querySelectorAll('.student-item');
 
 // Sets up the div for the page numbers.
 const page = document.querySelector('.page');
-const div = document.createElement('div');
-div.className = 'pagination';
+const div = createElement('div', 'className', 'pagination');
 page.appendChild(div);
 
-// Search
+// Sets up search section.
 const header = document.querySelector('.page-header');
-const searchDiv = document.createElement('div')
-searchDiv.className = 'student-search';
-const searchInput = document.createElement('input');
-searchInput.placeholder = 'Search for students...';
-const searchButton = document.createElement('button');
-searchButton.textContent = 'Search';
+const searchDiv = createElement('div', 'className', 'student-search');
+const searchInput = createElement('input', 'placeholder', 'Search for students...');
+const searchButton = createElement('button', 'textContent', 'Search');
+
 searchDiv.appendChild(searchInput);
 searchDiv.appendChild(searchButton);
 header.appendChild(searchDiv);
 
-const searchFromInput = (e) => {
-  const searchValue = e.target.value;
-  filterSearch(searchValue);
-}
+// Search Functions.
+const search = {
+  searchFromInput: (e) => {
+    const searchValue = e.target.value;
+    search.filterSearch(searchValue);
+  },
 
-const searchFromButton = (e) => {
-  const searchValue = searchInput.value;
-  filterSearch(searchValue);
-}
+  searchFromButton: (e) => {
+    const searchValue = searchInput.value;
+    search.filterSearch(searchValue);
+  },
 
-const filterSearch = (inputValue) => {
-  if (inputValue === '') {
-    removeError();
-    showPage(1);
-    appendPageLinks(liItems);
-  } else {
-    let results = [];
-    [...liItems].forEach((item) => {
-      if (item.textContent.includes(inputValue)) {
-        item.style.display = '';
-        results.push(item);
+  filterSearch: (inputValue) => {
+    // If nothing is in the search box.
+    if (inputValue === '') {
+      errors.removeError();
+      view.showPage(1);
+      view.appendPageLinks(liItems);
+    } else {
+      let results = [];
+      [...liItems].forEach((item) => {
+        if (item.textContent.includes(inputValue)) {
+          item.style.display = '';
+          results.push(item);
+        } else {
+          item.style.display = 'none';
+        }
+      })
+
+      // If the search returns no results.
+      if (results.length === 0) {
+        errors.showError();
+        view.appendPageLinks([]);
       } else {
+        errors.removeError();
+        view.appendPageLinks(results);
+      }
+    }
+  }
+}
+
+
+// Showing, appending and changing pages and page numbers.
+const view = {
+  // Hides all items except 10 depending on page number.
+  showPage: (pageNumber) => {
+    const ten = 10;
+    // Gets the start and end indexes.
+    const end = (pageNumber * ten) - 1;
+    const start = (end - ten) + 1;
+
+    // Hides all items not selected by the page number.
+    [...liItems].forEach((item, i) => {
+      if (i < start || i > end) {
         item.style.display = 'none';
+      } else {
+        item.style.display = '';
       }
     })
+  },
 
-    // If the search returns no results.
-    if (results.length === 0) {
-      showError();
-      appendPageLinks([]);
-    } else {
-      removeError();
-      appendPageLinks(results);
+  /* Generate, append, and add functionality to the pagination buttons.
+  Runs when the page first loads with the liItems array, and
+  is called when searching. */
+  appendPageLinks: (itemsArr) => {
+    // Removes any page buttons that were there.
+    if (div.children) {
+      [...div.children].forEach((child) => {
+        div.removeChild(child);
+      })
     }
+    // How many pages there should be.
+    const numberOfPages = Math.ceil(itemsArr.length / 10);
+    for (let pageNumber = 1; pageNumber <= numberOfPages; pageNumber++) {
+      const li = document.createElement('li');
+      // First page is highlighted when page loads or when search is blank.
+      const a = document.createElement('a');
+      if (pageNumber === 1) {
+        a.className = 'active';
+      } else {
+        a.className = 'inactive';
+      }
+      // Page loads to the top when a new page is selected.
+      a.href = '#';
+      a.textContent = pageNumber;
+
+      li.appendChild(a);
+      div.appendChild(li);
+      a.addEventListener('click', view.changePage);
+    }
+  },
+
+  // Changes the page when page button clicked.
+  changePage: (e) => {
+    const previousPage = document.querySelector('.active');
+    const pageNumber = e.target.textContent;
+    // Highlights the clicked button, and un-highlights the previous button.
+    e.target.className = 'active';
+    previousPage.className = 'inactive';
+
+    view.showPage(pageNumber);
   }
 }
+
 
 // Error message for no search results.
-const showError = () => {
-  // So the error doesn't keep repeating.
-  if (document.querySelector('.error')) {
-    removeError();
-  }
-  const p = document.createElement('p');
-  p.className = 'error';
-  p.textContent = `Oops, we couldn't find what you're looking for`
-  header.appendChild(p);
-}
-
-const removeError = () => {
-  if (document.querySelector('.error')) {
-    header.removeChild(document.querySelector('.error'));
-  }
-}
-
-searchInput.addEventListener('keyup', searchFromInput);
-searchButton.addEventListener('click', searchFromButton);
-
-
-// Hides all items except 10 depending on page number.
-const showPage = (pageNumber) => {
-  const ten = 10;
-  // Gets the start and end indexes.
-  const end = (pageNumber * ten) - 1;
-  const start = (end - ten) + 1;
-
-  // Hides all items not selected by the page number.
-  [...liItems].forEach((item, i) => {
-    if (i < start || i > end) {
-      item.style.display = 'none';
-    } else {
-      item.style.display = '';
+const errors = {
+  showError: () => {
+    if (document.querySelector('.error')) {
+      errors.removeError();
     }
-  })
-}
 
+    const p = createElement('p', 'textContent', `Oops! We can't find what you're looking for. Please try again.`);
+    p.className = 'error';
 
-// Generate, append, and add functionality to the pagination buttons. Runs only once.
-const appendPageLinks = (itemsArr) => {
-  // Checks if page buttons are there and removes them if they are.
-  if (div.children) {
-    [...div.children].forEach((child) => {
-      div.removeChild(child);
-    })
-  }
+    header.appendChild(p);
+  },
 
-  // How many pages there should be.
-  const numberOfPages = Math.ceil(itemsArr.length / 10);
-  for (let pageNumber = 1; pageNumber <= numberOfPages; pageNumber++) {
-    const li = document.createElement('li');
-    // First page is highlighted when page loads or when search is blank.
-    const a = document.createElement('a');
-    if (pageNumber === 1) {
-      a.className = 'active';
-    } else {
-      a.className = 'inactive';
+  removeError: () => {
+    if (document.querySelector('.error')) {
+      header.removeChild(document.querySelector('.error'));
     }
-    // Page loads to the top when a new page is selected.
-    a.href = '#';
-    a.textContent = pageNumber;
-
-    li.appendChild(a);
-    div.appendChild(li);
-    a.addEventListener('click', changePage);
   }
 }
 
-
-// Changes the page when button clicked.
-const changePage = (e) => {
-  const previousPage = document.querySelector('.active');
-  const pageNumber = e.target.textContent;
-  // Highlights the clicked button,
-  e.target.className = 'active';
-  // and un-highlights the previous button.
-  previousPage.className = 'inactive';
-  // Calls showPage to change the page.
-  showPage(pageNumber);
-}
+searchInput.addEventListener('keyup', search.searchFromInput);
+searchButton.addEventListener('click', search.searchFromButton);
 
 
 // Shows page 1 and buttons when page is first loaded.
-showPage(1);
-appendPageLinks(liItems);
+view.showPage(1);
+view.appendPageLinks(liItems);
